d83c2aa150a21e0e675fec9b0fc98528172a6994
akkoo <developerakshayjadhav@gmail.com>
2024-07-12 11:57:05 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'laundry-user',
+  webDir: 'www'
+};
+
+export default config;
